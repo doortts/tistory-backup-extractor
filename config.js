46bc686059cfcd50e537a077ee47c22ffd695648
Suwon Chae <doortts@gmail.com>
2016-11-20 20:53:02 +0900

    config: Update to default config

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 export default {
-  EXPORT_FILE_NAME: 'ti.xml',
+  EXPORT_FILE_NAME: 'tistory-backup.xml',
   YOUR_TISTORY_URL: 'http://blog.doortts.com',
   YOUR_NAME: 'doortts',
   HIDE_PRIVATE_POST: true,
   EXPORT_BASE_DIR: './blog',
   ATTACHMENTS_DIR: './attachments',
   YONA: {
-    DO_EXPORT: true,
-    SERVER: 'http://127.0.0.1:9000',
+    DO_EXPORT: false,
+    SERVER: 'https://repo.yona.io',
     USER_TOKEN: 'FUMwruLNFz0EAldNbXuMawqDl01gLp1XI0M7qPu1pX0=',
-    OWNER_NAME: 'doortts',
-    PROJECT_NAME: 'Test',
+    OWNER_NAME: 'FOO',
+    PROJECT_NAME: 'BAR',
     ROOT_CONTEXT: ''
   }
 };
